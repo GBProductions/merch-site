9cf8e81e7a9641ae40cb3d268e1c9a6dde0c4464
Garrett Brown <garrettpaulbrown@gmail.com>
2021-08-16 20:44:17 -0700

    Add hover text.
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

--- src/components/Gadget.js
+++ src/components/Gadget.js
@@ -5,7 +5,11 @@ function Gadget(props){
   return (
     <React.Fragment>
       <div onClick = {() => props.whenGadgetClicked(props.id)}>
-        <p><strong>Gadget Name: </strong> {props.name}</p>
+
+      <div class="tooltip"><p><strong>Gadget Name: </strong> {props.name}</p>
+        <span class="tooltiptext">click to see details</span>
+      </div>
+        {/* <p><strong>Gadget Name: </strong> {props.name}</p> */}
         {/* <p><strong>Quantity Available: </strong> {props.quantity}</p> */}
         <hr />
       </div>
